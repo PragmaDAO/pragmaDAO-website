4a8b9b58084996b4065d99a6310f7bd167d79030
FaceyMcFacey <FaceyMcFaceyStien@proton.me>
2025-08-07 17:24:46 -0400

    Updates

---
+++ lessons/solidity/Lesson3.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract SimpleFunctions {
+    // Here is a basic function that will return a number.
+    function getNumber() public pure returns (uint) {
+        return 1;
+    }
+}
