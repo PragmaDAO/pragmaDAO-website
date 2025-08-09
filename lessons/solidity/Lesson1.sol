13f4fc659e751d26c2e23866203582d825bffe60
FaceyMcFacey <FaceyMcFaceyStien@proton.me>
2025-08-08 20:54:14 -0400

    Updates

--- lessons/solidity/Lesson1.sol
+++ lessons/solidity/Lesson1.sol
@@ -1,8 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.26;
 
 contract HelloWorld {
-    function greet() public pure returns (string memory) {
-        return "Hello, World!";
+    // Declare a public state variable of type string named greeting
+    string public greeting;
+
+    function greet() public returns (string memory) {
+        // Initialize the greeting variable with a value
+        greeting = "Hello, World!";
+        return greeting;
     }
 }
