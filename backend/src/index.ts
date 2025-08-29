fcf31689077fa2694332f23ff61f69a4a327a161
FaceyMcFacey <FaceyMcFaceyStien@proton.me>
2025-08-28 20:14:59 -0400

    fixed basics for database using prisma. Added routes and migrations.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,14 @@
 import express, { Express, Request, Response } from 'express';
+import authRouter from './routes/auth';
 
 const app: Express = express();
 const port = 3001;
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Use the authentication router
+app.use('/api/auth', authRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World from the backend!');
